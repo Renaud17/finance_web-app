873cb91b2f4a725366eb6d396477dd8ca553943c
Gordon Pisciotta <gdp011235@gmail.com>
2021-04-20 14:58:49 -0600

    Update hub.py

--- hub.py
+++ hub.py
@@ -524,77 +524,77 @@ if(systemStage == '3-Technical-Analysis'):
                 return x['name']
     technical_company = get_symbol(ticker)
 
-  st.sidebar.subheader('> Step #3')
-  st.sidebar.markdown(f"Hit 'Run' For Technical Analysis On:\n\n {technical_company} ({ticker})")
-  run_button = st.sidebar.button("RUN")
+    st.sidebar.subheader('> Step #3')
+    st.sidebar.markdown(f"Hit 'Run' For Technical Analysis On:\n\n {technical_company} ({ticker})")
+    run_button = st.sidebar.button("RUN")
 
-  if run_button:
-    st.subheader('Moving Average')
-    coMA1, coMA2 = st.beta_columns(2)
-    with coMA1:
-        numYearMA = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=0)    
-    with coMA2:
-        windowSizeMA = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=1)  
+    if run_button:
+        st.subheader('Moving Average')
+        coMA1, coMA2 = st.beta_columns(2)
+        with coMA1:
+            numYearMA = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=0)    
+        with coMA2:
+            windowSizeMA = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=1)  
         
-    start = datetime.today()-timedelta(numYearMA * 365)
-    end = datetime.today()
-    
-    dataMA = yf.download(ticker,start,end)
-    df_ma = calcMovingAverage(dataMA, windowSizeMA)
-    df_ma = df_ma.reset_index()
-    
-    figMA = go.Figure()    
-    figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['Adj Close'], name="Prices Over Last "+str(numYearMA)+" Year(s)"))    
-    figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['sma'], name="SMA"+str(windowSizeMA)+" Over Last "+str(numYearMA)+" Year(s)"))
-    figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['ema'], name="EMA"+str(windowSizeMA)+" Over Last "+str(numYearMA)+" Year(s)"))
-    figMA.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
-    figMA.update_layout(legend_title_text='Trend')
-    figMA.update_yaxes(tickprefix="$")
-    st.plotly_chart(figMA, use_container_width=True)  
-    
-    st.subheader('Moving Average Convergence Divergence (MACD)')
-    numYearMACD = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=2) 
-    startMACD = datetime.today()-timedelta(numYearMACD * 365)
-    endMACD = datetime.today()
-    dataMACD = yf.download(ticker,startMACD,endMACD)
-    df_macd = calc_macd(dataMACD)
-    df_macd = df_macd.reset_index()
-    
-    figMACD = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.01)
-    figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['Adj Close'], name = "Prices Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
-    figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['ema12'], name = "EMA 12 Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
-    figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['ema26'], name = "EMA 26 Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
-    figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['macd'], name = "MACD Line"), row=2, col=1)
-    figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['signal'], name = "Signal Line"), row=2, col=1)
-    figMACD.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=0))
-    figMACD.update_yaxes(tickprefix="$")
-    st.plotly_chart(figMACD, use_container_width=True)
-    
-    st.subheader('Bollinger Band')
-    coBoll1, coBoll2 = st.beta_columns(2)
-    with coBoll1:
-        numYearBoll = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=6) 
-    with coBoll2:
-        windowSizeBoll = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=7)
-        
-    startBoll= datetime.today()-timedelta(numYearBoll * 365)
-    endBoll = datetime.today()
-    dataBoll = yf.download(ticker,startBoll,endBoll)
-    df_boll = calcBollinger(dataBoll, windowSizeBoll)
-    df_boll = df_boll.reset_index()
-    
-    figBoll = go.Figure()
-    figBoll.add_trace(go.Scatter(x = df_boll['Date'], y = df_boll['bolu'], name = "Upper Band"))
-    figBoll.add_trace(go.Scatter(x=df_boll['Date'], y=df_boll['sma'], name="SMA"+str(windowSizeBoll)+" Over Last "+str(numYearBoll)+" Year(s)"))
-    figBoll.add_trace(go.Scatter(x = df_boll['Date'], y = df_boll['bold'], name = "Lower Band"))
-    figBoll.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=0))
-    figBoll.update_yaxes(tickprefix="$")
-    st.plotly_chart(figBoll, use_container_width=True)
-    st.write(' *'*25)
-    st.subheader('- To Work In A Different Analysis Category:')
-    st.write('* Go To Step #1')
-    st.subheader('- To Use Other Models Within This Same Analysis Category:')
-    st.write('* Go To Step #2')
+        start = datetime.today()-timedelta(numYearMA * 365)
+        end = datetime.today()
+
+        dataMA = yf.download(ticker,start,end)
+        df_ma = calcMovingAverage(dataMA, windowSizeMA)
+        df_ma = df_ma.reset_index()
+
+        figMA = go.Figure()    
+        figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['Adj Close'], name="Prices Over Last "+str(numYearMA)+" Year(s)"))    
+        figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['sma'], name="SMA"+str(windowSizeMA)+" Over Last "+str(numYearMA)+" Year(s)"))
+        figMA.add_trace(go.Scatter(x=df_ma['Date'], y=df_ma['ema'], name="EMA"+str(windowSizeMA)+" Over Last "+str(numYearMA)+" Year(s)"))
+        figMA.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01))
+        figMA.update_layout(legend_title_text='Trend')
+        figMA.update_yaxes(tickprefix="$")
+        st.plotly_chart(figMA, use_container_width=True)  
+
+        st.subheader('Moving Average Convergence Divergence (MACD)')
+        numYearMACD = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=2) 
+        startMACD = datetime.today()-timedelta(numYearMACD * 365)
+        endMACD = datetime.today()
+        dataMACD = yf.download(ticker,startMACD,endMACD)
+        df_macd = calc_macd(dataMACD)
+        df_macd = df_macd.reset_index()
+
+        figMACD = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.01)
+        figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['Adj Close'], name = "Prices Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
+        figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['ema12'], name = "EMA 12 Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
+        figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['ema26'], name = "EMA 26 Over Last " + str(numYearMACD) + " Year(s)"), row=1, col=1)
+        figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['macd'], name = "MACD Line"), row=2, col=1)
+        figMACD.add_trace(go.Scatter(x = df_macd['Date'], y = df_macd['signal'], name = "Signal Line"), row=2, col=1)
+        figMACD.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=0))
+        figMACD.update_yaxes(tickprefix="$")
+        st.plotly_chart(figMACD, use_container_width=True)
+
+        st.subheader('Bollinger Band')
+        coBoll1, coBoll2 = st.beta_columns(2)
+        with coBoll1:
+            numYearBoll = st.number_input('Insert period (Year): ', min_value=1, max_value=10, value=2, key=6) 
+        with coBoll2:
+            windowSizeBoll = st.number_input('Window Size (Day): ', min_value=5, max_value=500, value=20, key=7)
+
+        startBoll= datetime.today()-timedelta(numYearBoll * 365)
+        endBoll = datetime.today()
+        dataBoll = yf.download(ticker,startBoll,endBoll)
+        df_boll = calcBollinger(dataBoll, windowSizeBoll)
+        df_boll = df_boll.reset_index()
+
+        figBoll = go.Figure()
+        figBoll.add_trace(go.Scatter(x = df_boll['Date'], y = df_boll['bolu'], name = "Upper Band"))
+        figBoll.add_trace(go.Scatter(x=df_boll['Date'], y=df_boll['sma'], name="SMA"+str(windowSizeBoll)+" Over Last "+str(numYearBoll)+" Year(s)"))
+        figBoll.add_trace(go.Scatter(x = df_boll['Date'], y = df_boll['bold'], name = "Lower Band"))
+        figBoll.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="left", x=0))
+        figBoll.update_yaxes(tickprefix="$")
+        st.plotly_chart(figBoll, use_container_width=True)
+        st.write(' *'*25)
+        st.subheader('- To Work In A Different Analysis Category:')
+        st.write('* Go To Step #1')
+        st.subheader('- To Use Other Models Within This Same Analysis Category:')
+        st.write('* Go To Step #2')
 
 
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
