b765ab401875eaa9015a8b72d9495e869a4564e3
Gordon Pisciotta <gordon.pisciotta@gmail.com>
2021-04-22 21:43:04 -0600

    arima

--- pages/forecast/__init__.py
+++ pages/forecast/__init__.py
@@ -4,4 +4,4 @@ from .web_sarima import The_SARIMA_Model as sarima
 from .web_univariate import The_Univariate_TS_Reg as univariate
 from .web_stocker import web_stocker_run
 from .stocker import Stocker
-# from .web_arima import Web_Arima, The_Arima_Model
+from .web_arima import Web_Arima, The_Arima_Model
