017d1ebeee0b1eb907b1b1c37363b92b667c6645
Gordon Pisciotta <gordon.pisciotta@gmail.com>
2021-04-22 21:58:42 -0600

    new

--- pages/backtest/__init__.py
+++ pages/backtest/__init__.py
@@ -1,5 +1,5 @@
-from .web_backtrader_sma_strategy import backtrader_sma_strategy_run
-from .backTrader_BuyHold import xavier as buyHold
+# from .web_backtrader_sma_strategy import backtrader_sma_strategy_run
+# from .backTrader_BuyHold import xavier as buyHold
 from .web_one import Web_One
 from .vectorized_backtest import The_Vectorized_Backtest
 from .portfolio_analysis import Analize_Portfolio
